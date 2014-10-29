c89c8bfde7cf9b67457b138cfb1c174c5e19db20
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-10-29 19:47:47 -0400

    more comms

--- dist/scripts/background.js
+++ dist/scripts/background.js
@@ -1,18 +1,20 @@
 'use strict';
 
-var toggle = false;
-chrome.browserAction.onClicked.addListener(function (tab) {
-    toggle = !toggle;
-    if (toggle) {
-        chrome.browserAction.setIcon({path: {
-            '38': "images/icon-38-on.png"
-        }, tabId: tab.id});
-        // chrome.tabs.executeScript(tab.id, {file: "SCRIPT.user.js"});
-    }
-    else {
-        chrome.browserAction.setIcon({path: {
-            '38': "images/icon-38-off.png"
-        }, tabId: tab.id});
-        // chrome.tabs.executeScript(tab.id, {code: "alert()"});
-    }
-});
+/*
+ var toggle = true;
+ chrome.browserAction.onClicked.addListener(function (tab) {
+ toggle = !toggle;
+ if (!toggle) {
+ chrome.browserAction.setIcon({path: {
+ '38': "images/icon-38-on.png"
+ }, tabId: tab.id});
+ chrome.tabs.executeScript(tab.id, {file: "scripts/inject.js"});
+ }
+ else {
+ chrome.browserAction.setIcon({path: {
+ '38': "images/icon-38-off.png"
+ }, tabId: tab.id});
+ }
+ });
+
+ */
