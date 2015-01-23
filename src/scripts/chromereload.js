a843aa9dd8f9a1d5df9e6a196c1e24fc2f37e35d
Marko Raatikka <marko.raatikka@gmail.com>
2015-01-23 23:26:17 +0200

    Remove left over gulp-clean from package.json

--- src/scripts/chromereload.js
+++ src/scripts/chromereload.js
@@ -1,26 +1,26 @@
 (function () {
 
-	'use strict';
+    'use strict';
 
-	// Reload client for Chrome Apps & Extensions.
-	// The reload client has a compatibility with livereload.
-	// WARNING: only supports reload command.
+    // Reload client for Chrome Apps & Extensions.
+    // The reload client has a compatibility with livereload.
+    // WARNING: only supports reload command.
 
-	var LIVERELOAD_HOST = 'localhost:';
-	var LIVERELOAD_PORT = 35729;
-	var connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
+    var LIVERELOAD_HOST = 'localhost:';
+    var LIVERELOAD_PORT = 35729;
+    var connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
 
-	connection.onerror = function (error) {
-	    console.log('reload connection got error' + JSON.stringify(error));
-	};
+    connection.onerror = function (error) {
+        console.log('reload connection got error' + JSON.stringify(error));
+    };
 
-	connection.onmessage = function (e) {
-	    if (e.data) {
-	        var data = JSON.parse(e.data);
-	        if (data && data.command === 'reload') {
-	            chrome.runtime.reload();
-	        }
-	    }
-	};
+    connection.onmessage = function (e) {
+        if (e.data) {
+            var data = JSON.parse(e.data);
+            if (data && data.command === 'reload') {
+                chrome.runtime.reload();
+            }
+        }
+    };
 
 })();
