fe9926cd9779337cc8fa066ab87fa0e0cda09430
junv <wahyd4@gmail.com>
2015-03-26 21:49:47 +0800

    Add support for mocking Toast-Phonegap-Plugin. https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin

---
+++ src/plugins/toast.js
@@ -0,0 +1,32 @@
+function Toast() {
+}
+
+Toast.prototype.show = function (message, duration, position, successCallback, errorCallback) {
+  console.log("Toast has been called, message:" + message + ", duration:" + duration + ", position: " + position)
+};
+
+Toast.prototype.showShortTop = function (message, successCallback, errorCallback) {
+  this.show(message, "short", "top", successCallback, errorCallback);
+};
+
+Toast.prototype.showShortCenter = function (message, successCallback, errorCallback) {
+  this.show(message, "short", "center", successCallback, errorCallback);
+};
+
+Toast.prototype.showShortBottom = function (message, successCallback, errorCallback) {
+  this.show(message, "short", "bottom", successCallback, errorCallback);
+};
+
+Toast.prototype.showLongTop = function (message, successCallback, errorCallback) {
+  this.show(message, "long", "top", successCallback, errorCallback);
+};
+
+Toast.prototype.showLongCenter = function (message, successCallback, errorCallback) {
+  this.show(message, "long", "center", successCallback, errorCallback);
+};
+
+Toast.prototype.showLongBottom = function (message, successCallback, errorCallback) {
+  this.show(message, "long", "bottom", successCallback, errorCallback);
+};
+
+window.plugins.toast = new Toast();
