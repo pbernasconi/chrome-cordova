8689665b782809053298f6a501a2d0080a1d3b08
vgrafe <vincent.grafe@gmail.com>
2015-03-31 20:01:22 -0400

    added support for splashScreen

---
+++ src/plugins/splashScreen.js
@@ -0,0 +1,14 @@
+/****************************************************************
+ * SplashScreen
+ */
+
+if (!navigator.splashscreen)
+    navigator.splashscreen = {};
+
+navigator.splashscreen.show = function() {
+    return true;
+};
+
+navigator.splashscreen.hide = function() {
+    return true;
+};
